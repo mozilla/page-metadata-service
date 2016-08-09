544efa8021e222c7e000c6e7bb4e134e5de9c165
Peter deHaan <peter@deseloper.com>
2016-08-08 19:12:50 -0700

    Clean up some ESLint and Promise handling

--- app/service.js
+++ app/service.js
@@ -5,14 +5,12 @@ const fetch = require('node-fetch');
 const urlparse = require('url');
 const {getMetadata} = require('page-metadata-parser');
 
-
 function buildObj(pairs) {
   return pairs.reduce((newObj, [key, value]) => {
     newObj[key] = value;
     return newObj;
   }, {});
 }
-
 
 function getDocumentMetadata(url, window) {
   const doc = window.document;
@@ -24,36 +22,29 @@ function getDocumentMetadata(url, window) {
 
   if (!metadata.favicon_url) {
     const parsedUrl = urlparse.parse(url);
-    metadata.favicon_url = parsedUrl.protocol + '//' + parsedUrl.host + '/favicon.ico';
+    metadata.favicon_url = `${parsedUrl.protocol}//${parsedUrl.host}/favicon.ico`;
   }
 
   metadata.image_url = metadata.image_url && metadata.image_url.replace(/^\/\//, 'https://');
   metadata.images = [{
-    'url': metadata.image_url,
-    'width': 500,
-    'height': 500,
-    'entropy': 1.0
+    entropy: 1.0,
+    height: 500,
+    url: metadata.image_url,
+    width: 500,
   }];
 
-  console.log('Generated Metadata for ' + url + ':\n' + JSON.stringify(metadata));
+  console.log(`Generated Metadata for ${url}:\n${JSON.stringify(metadata)}`); // eslint-disable-line no-console
 
   return metadata;
 }
 
 
 function getUrlMetadata(url) {
-  return new Promise((resolve, reject) => {
-    fetch(url).then((res) => {
-      return res.text();
-    }).then((body) => {
-      const window = domino.createWindow(body);
-      resolve(getDocumentMetadata(url, window));
-    }).catch((err) => {
-      resolve({});
-    });
-  });
+  return fetch(url)
+    .then((res) => res.text())
+    .then((body) => getDocumentMetadata(url, domino.createWindow(body)))
+    .catch((err) => ({}));
 }
-
 
 const app = express();
 app.use(bodyParser.json()); // for parsing application/json
@@ -61,27 +52,26 @@ app.use(bodyParser.json()); // for parsing application/json
 app.post('/', function(req, res) {
   const response = {
     error: '',
-    urls: [],
-  }
-
-  function fail(reason) {
+    urls: []
+  };
+  const fail = (reason) => {
     response.error = reason;
     res.json(response);
-  }
+  };
 
-  if(!req.body.urls) {
-    fail('Unable to locate `urls` in body: ' + JSON.stringify(req.body));
+  if (!req.body.urls) {
+    fail(`Unable to locate 'urls' in body: ${JSON.stringify(req.body)}`);
     return;
   }
 
-  const promises = req.body.urls.map((url) => {
-    return getUrlMetadata(url);
-  });
+  const promises = req.body.urls.map((url) => getUrlMetadata(url));
 
-  Promise.all(promises).then((urlsData) => {
-    response.urls = buildObj(urlsData.map((urlData) => [urlData.url, urlData]));
-    res.json(response);
-  });
+  Promise.all(promises)
+    .then((urlsData) => {
+      response.urls = buildObj(urlsData.map((urlData) => [urlData.url, urlData]));
+      res.json(response);
+    })
+    .catch((err) => fail(err.message));
 });
 
 app.listen(7001, function() {
