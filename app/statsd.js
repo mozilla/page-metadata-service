6321c5d9de5273b412d4c510f81fcb9e2e1ec1e9
Jared Kerim <jared.l.kerim@gmail.com>
2016-09-02 19:45:53 +0000

    Send ms to statsd #92

--- app/statsd.js
+++ app/statsd.js
@@ -3,7 +3,7 @@ const statsd = require('node-statsd');
 const statsdClient = new statsd({host: process.env.STATSD_HOST});
 
 statsdClient.getTimestamp = function() {
-  return Math.floor(new Date() / 1000);
+  return Math.floor(new Date());
 };
 
 module.exports = statsdClient;
